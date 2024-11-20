7661481b1e3778014cc5bf91b5596af3e5ea6fdb
StoneCAU <seokwoo1999@naver.com>
2024-11-20 14:32:22 +0900

    feat: 자판기 보유 금액 입력 구현

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,8 +1,17 @@
 package vendingmachine.controller;
 
+import vendingmachine.validator.InputValidator;
+import vendingmachine.view.InputView;
+
 public class VendingMachineController {
 
     public void run() {
+        int initialMoney = getMoney();
+    }
 
+    private int getMoney() {
+        String input = InputView.inputInitialMoney();
+
+        return InputValidator.validateMoney(input);
     }
 }
