16622723889a3709bffee85c1706a6ddbdd0a1b9
StoneCAU <seokwoo1999@naver.com>
2024-11-20 14:32:06 +0900

    feat(InputView): 자판기 보유 금액 입력 메서드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,9 +1,16 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private final static String NEW_LINE = System.lineSeparator();
 
     private void printNewLine() {
         System.out.printf(NEW_LINE);
+    }
+
+    public static String inputInitialMoney() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        return Console.readLine();
     }
 }
